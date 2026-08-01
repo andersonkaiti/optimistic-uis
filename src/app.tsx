86c8e919a72ef98ea3c3beeef10f9a1b40e4d51b
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-31 21:42:45 -0300

    :tada: chore(init): setup project

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-zinc-950">
+      <h1 className="text-white">Hello, World!</h1>
+    </div>
+  )
+}
