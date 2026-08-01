a00e7f77f09ce0c19b6debb46ad5a8925530f9a3
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-01 17:22:01 -0300

    :sparkles: feat(query): setup react query with query client and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import { ThemeProvider } from '@contexts/theme-context.tsx'
+import { queryClient } from '@lib/query-client.ts'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
@@ -6,8 +9,12 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 )
